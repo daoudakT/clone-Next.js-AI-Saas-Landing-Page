89ca1e4371e9f81a6fd4a3d333557e4e8b962554
daoud <daoudakone532@gmail.com>
2025-03-26 14:44:18 +0100

    envoie du projet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import Header from "@/sections/Header";
+import Hero from "@/sections/Hero";
+import Companies from "@/sections/Companies";
+import Features from "@/sections/Features";
+import Pricing from "@/sections/Pricing";
+import Testimonials from "@/sections/Testimonials";
+import CallToAction from "@/sections/CallToAction";
+import Footer from "@/sections/Footer";
+
 export default function Home() {
-  return <div>hello world</div>;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Companies />
+      <Features />
+      <Pricing />
+      <Testimonials />
+      <CallToAction />
+      <Footer />
+    </>
+  );
 }
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import AshwinSantiago from "@/assets/images/ashwin-santiago.jpg";
 import AlecWhitten from "@/assets/images/alec-whitten.jpg";
 import ReneWells from "@/assets/images/rene-wells.jpg";
 import MollieHall from "@/assets/images/mollie-hall.jpg";
+import { SectionBorder } from "@/components/SectionBorder";
+import { SectionContent } from "@/components/SectionContent";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+import { AnimatePresence, LayoutGroup, motion } from "framer-motion";
 
 export const testimonials = [
   {
@@ -36,7 +43,80 @@ export const testimonials = [
 ];
 
 export const Testimonials = () => {
-  return <section>testimonials section</section>;
+  const [testimonialIndex, setTestimonialIndex] = useState(0);
+  return (
+    <section id="testimonials">
+      <div className="container">
+        <SectionBorder borderTop>
+          <SectionContent>
+            <LayoutGroup>
+              <motion.div
+                layout
+                className="border-gradient rounded-3xl px-6 md:px-10 lg:px-16 py-16 lg:py-24 relative flex flex-col md:flex-row gap-12 md:mx-10 lg:mx-20"
+              >
+                <FontAwesomeIcon
+                  icon={faQuoteLeft}
+                  className="absolute size-20 text-violet-400 top-0 left-6 md:left-10 lg:left-16 -translate-y-1/2"
+                />
+                <AnimatePresence mode="wait" initial={false}>
+                  {testimonials.map((testimonial, index) =>
+                    testimonialIndex === index ? (
+                      <motion.blockquote
+                        initial={{ opacity: 0, y: 25 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: 25 }}
+                        transition={{ duration: 0.5 }}
+                        key={testimonial.name}
+                        layout
+                        className="flex flex-col lg:flex-row gap-12"
+                      >
+                        <p className="text-xl md:text-2xl font-medium">
+                          {testimonial.quote}
+                        </p>
+                        <cite className="not-italic lg:text-right">
+                          <Image
+                            src={testimonial.image}
+                            alt={testimonial.name}
+                            className="rounded-xl size-28 max-w-none"
+                          />
+                          <div className="font-bold mt-4">
+                            {testimonial.name}
+                          </div>
+                          <div className="text-xs text-gray-400 mt-2">
+                            {testimonial.title}
+                          </div>
+                        </cite>
+                      </motion.blockquote>
+                    ) : null
+                  )}
+                </AnimatePresence>
+                <motion.div
+                  layout="position"
+                  className="flex gap-2 md:flex-col"
+                >
+                  {testimonials.map((testimonial, index) => (
+                    <div
+                      key={testimonial.name}
+                      className="size-6 relative isolate inline-flex items-center justify-center"
+                      onClick={() => setTestimonialIndex(index)}
+                    >
+                      {testimonialIndex === index && (
+                        <motion.div
+                          className="absolute border-gradient size-full rounded-full -z-10"
+                          layoutId="testimonial-dot"
+                        ></motion.div>
+                      )}
+                      <div className="size-1.5 bg-gray-200 rounded-full"></div>
+                    </div>
+                  ))}
+                </motion.div>
+              </motion.div>
+            </LayoutGroup>
+          </SectionContent>
+        </SectionBorder>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
